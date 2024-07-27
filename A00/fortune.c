2b06574f1391c7f341e28d1a09a60838122fc820
Fizzy <aidenrivera7@gmail.com>
2024-07-27 00:40:41 -0400

    0th assignment

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * fortune.c
+ * Author: Fizzy 
+ * Date: 7/27/24 
+ * Print "A journey of a thousand miles begins with a single step." 
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("A journey of a thousand miles begins with a single step.\n");
+  return 0;
+}
